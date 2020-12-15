907c28098c6715f879a09917d9ff2feb31295542
John Park <spacecwby417@gmail.com>
2020-12-15 17:45:53 -0500

    Fetching data from the movie database api

---
+++ script.js
@@ -0,0 +1,30 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=06790d83eada521989c0c57e82590366&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=06790d83eada521989c0c57e82590366&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+// Get initial movies
+getMovies(API_URL)
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log(data.results);    
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
